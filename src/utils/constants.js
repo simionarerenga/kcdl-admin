ce21705f2e9ad8dbdca9573fadaaeaf27a08663b
simionarerenga <simionarerenga@gmail.com>
2026-05-06 13:04:58 +1100

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,29 @@
+// src/utils/constants.js
+// Shared constants used across the app
+
+export const KIRIBATI_ISLANDS = [
+  'Betio Town',
+  'Bairiki (Teinainano Urban)',
+  'Eutan Tarawa',
+  'Abaiang',
+  'Abemama',
+  'Aranuka',
+  'Arorae',
+  'Banaba (Ocean Island)',
+  'Beru',
+  'Butaritari',
+  'Kiritimati (Christmas Island)',
+  'Kuria',
+  'Maiana',
+  'Makin',
+  'Marakei',
+  'Nikunau',
+  'Nonouti',
+  'Onotoa',
+  'Tabiteuea North',
+  'Tabiteuea South',
+  'Tabuaeran (Fanning Island)',
+  'Tamana',
+  'Tarawa',
+  'Teraina (Washington Island)',
+];
